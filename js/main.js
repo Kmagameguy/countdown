e5d0f9d526a906b1864108aa8ae46936bf53c1c9
Kevin A <kmagameboy@gmail.com>
2017-12-01 16:12:39 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function loadTimer() {
-  var deadline = new Date("Dec 1, 2017 08:32:00").getTime();
+  var deadline = new Date("Dec 1, 2017 17:30:00").getTime();
 
   var x = setInterval(function() {
     var now = new Date().getTime();
