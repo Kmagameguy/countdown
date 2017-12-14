d6dfa57e09a889152674e13c469382a6ee5a3416
Kevin A <kmagameboy@gmail.com>
2017-12-13 20:17:55 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ function loadTimer() {
 
     bgMusic[0] = '<iframe width="20" height="20" src="https://www.youtube.com/embed/VtWxBwBLS9U?autoplay=1" style="visibility:hidden;"></iframe>';
     bgMusic[1] = '<iframe width="100%" height="300" scrolling="no" style="visibility:hidden;" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/122570957&amp;color=%23ff5500&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true&amp;visual=true"></iframe>';
-    bgMusic[2] = '<iframe width="100%" height="300" scrolling="no" style="visbility:hidden;" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/89178062&amp;color=%23ff5500&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true&amp;visual=true"></iframe>'
+    bgMusic[2] = '<iframe width="100%" height="300" scrolling="no" style="visibility:hidden;" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/89178062&amp;color=%23ff5500&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true&amp;visual=true"></iframe>'
 
     imageIndex = Math.floor( Math.random() * coverImages.length );
     musicIndex = Math.floor( Math.random() * bgMusic.length );
