ea00f28adaf980c5001e0c449ca450e996acbce2
Kevin A <kmagameboy@gmail.com>
2018-02-08 13:58:25 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function loadTimer() {
 // TO DO
 // Add Ball-OUT button to generate automatic emails
-  var deadline = new Date("Feb 7, 2018 16:15:00").getTime();
+  var deadline = new Date("Feb 8, 2018 16:15:00").getTime();
   var coverImages = [];
   var bgMusic = [];
   var quotes = [];
